18a394b1ce1520de9f246947b9608827a0208b7d
ousttrue <ousttrue@gmail.com>
2022-01-20 09:51:48 +0900

    find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
-setup(name='rawtypes',
-      version='1.0',
-      package_dir={'': 'src'},
-      packages=['rawtypes']
-      )
+import setuptools
+setuptools.setup(name='rawtypes',
+                 version='1.0',
+                 package_dir={'': 'src'},
+                 packages=setuptools.find_packages("src"),
+                 )
